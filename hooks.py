61ffaf92d8ce91ee9a01090af6e0e191e9fa4bb5
Kyle Bruder <38113226+kylebruder@users.noreply.github.com>
2021-07-04 16:49:29 -0700

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+# Hooks
+hooks = [
+        # Lookaway
+        "https://discord.com/api/webhooks/861312467577470977/B8seXAjC1AC73qrlPt3Edj03oflI-Sih0olx13cqHcHJU6yKJlF2rlnsw20Fs0d1EgfU",
+        # TTM Devs
+        "https://discord.com/api/webhooks/861390493997203467/PewjN-l-yus-yJVBpppyi_DVGf_eT488RRsecP0N6CwdpF7irVYgDTo2mA7is9q25ANj",
+]
+
+
+# Bitcoin price index via Coindesk
+
+bpi = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+date = bpi['time']['updated']
+disclaimer = bpi['disclaimer']
+price = bpi['bpi']['USD']['rate']
+
+content = """
+As of {}, the USD market value of Bitcoin was ${}.\n
+Disclaimer:
+{}
+""".format(date, price, disclaimer)
+
+# Build message
+message = {
+        'content': content,
+        }
+# Check to make sure the content will fit
+if len(message['content']) <= 2000:
+    for h in hooks:
+        r = requests.post(h, json=message)
+        #print(h)
+        #print(content)    
+else:
+    raise Exception("Content is too long! Must be less than 2000 characters")
+#print(r.status_code)
