5be000ef8ff3dff0a0a79647eb7870811a7b1e6c
Kyle Bruder <38113226+kylebruder@users.noreply.github.com>
2021-07-04 20:52:17 -0700

    add delay option; refactor

--- hooks.py
+++ hooks.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import time
 from datetime import datetime
 
 # Hooks
@@ -10,15 +11,18 @@ hooks = []
 # Number of spams
 n = 100
 
-def generate_timestamp_message():
+# Delay between messages in seconds
+delay = 0 
+
+def generate_timestamp_message(delay=0):
     '''
     Returns a json message with a timestamp.
     For testing purposes.
     '''
     time = datetime.now()
     content = """
-    generated on roadsauce at {}
-    """.format(time)
+    generated on roadsauce at {} - delay after POST (seconds): {}
+    """.format(time, delay)
 
     # Build message
     message = {
@@ -27,16 +31,26 @@ def generate_timestamp_message():
     return message
 
 # Check to make sure the content will fit
-for h in hooks:
-    for x in range(n):
-        message = generate_timestamp_message()
-        if len(message['content']) <= 2000:
-            r = requests.post(h, json=message)
-            #print(h)
-            #print(content)    
-            print("response code: {} - response time (seconds): {}".format(
-                r.status_code,
-                r.elapsed.total_seconds()
-            ))
-        else:
-            raise Exception("Content is too long! Must be less than 2000 characters")
+def spam_hooks(delay=0):
+    for h in hooks:
+        for x in range(n):
+            message = generate_timestamp_message(delay=delay)
+            if len(message['content']) <= 2000:
+                r = requests.post(h, json=message)
+                time.sleep(delay)
+                #print(h)
+                #print(content)    
+                print("{} - response code: {} - response time (seconds): {}".format(
+                    x,
+                    r.status_code,
+                    r.elapsed.total_seconds()
+                ))
+            else:
+                raise Exception("Content is too long! Must be less than 2000 characters")
+
+
+def main():
+    spam_hooks(delay=delay)
+
+if __name__ == "__main__":
+    main()
